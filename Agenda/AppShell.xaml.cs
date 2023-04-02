5482f4f609e37eb036fbdaa98d6c2f68d740c872
gzavlanis <gzavlanis@gmail.com>
2023-04-02 21:27:38 +0300

    Add project files.

---
+++ Agenda/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+	}
+}
